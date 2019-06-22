6efb38dfd453de17fd2b451900fa484ba0703737
hao.huang <hao.huang@aliyun.com>
2019-06-22 09:59:06 +0800

    init

---
+++ jsonp/remote.php
@@ -0,0 +1,7 @@
+<?php
+//通过设置content-type能够指明返回的内容类型
+header('Contetn-type:application/json');
+
+$callbackFunction = htmlspecialchars($_GET['callback']);
+$data = 'data from remote';
+echo $callbackFunction.'("'.$data.'")';
---
+++ nginx/server/server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var cors = require("cors");
+var app = express();
+
+//使用express的cors中间件使其支持跨域请求
+// app.use(cors());
+
+app.get("/api", function(req, res, next) {
+  res.json({ msg: "This is CORS-enabled for all origins!" });
+});
+
+app.listen(3000, function() {
+  console.log("CORS-enabled web server listening on port 3000");
+});
